ed7d4fb5f211be8ae0d74ccdaf51c8d24572e995
fitoji <fitoji@gmail.com>
2026-06-29 16:51:00 +0200

    feat: superchat amount text in white for contrast

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -567,7 +567,7 @@ function Index() {
                   </div>
                 </div>
                 {isSC && fullScreenMessage.superChatAmount && (
-                  <span className="flex-shrink-0 px-3 py-1 rounded-full bg-black/15 font-bold text-lg text-black">
+                  <span className="flex-shrink-0 px-3 py-1 rounded-full bg-black/15 font-bold text-lg text-white">
                     {fullScreenMessage.superChatAmount}
                   </span>
                 )}
